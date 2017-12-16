7d665061aec99afac1575bb8660b3b7b2da14fd9
vinaykornapalli <vinaykornapalli@gmail.com>
2017-12-16 19:49:29 +0530

    master

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+var typed = new Typed('#main-head', {
+    strings: ["We care", "The Green Movement"],
+    typeSpeed: 100,
+    shuffle:true,
+    smartBackspace: false,
+    loop: true
+    
+});
+
+
